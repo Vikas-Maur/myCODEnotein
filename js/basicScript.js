11cbf34381a63e3799c5fa9d2b8b98c7a5341d9e
Vikas <vikasmaur10@gmail.com>
2020-10-12 14:15:31 +0530

    FEW MORE BUG FIXES

--- js/basicScript.js
+++ js/basicScript.js
@@ -1,13 +1,1 @@
-const heading = document.querySelector(".heading");
-const headingAnimationName = heading.getAttribute("data-animation") || "headingBubble";
 
-function HeadingAnimation(){
-    const navAnimationPlay = document.querySelector("nav").getAttribute("data-animationPlayed");
-    if (navAnimationPlay == "false") {
-        heading.style.setProperty("animation", `${headingAnimationName} 3s ease-in-out`);
-    }
-    else {
-        heading.style.setProperty("animation", `${headingAnimationName} 3s ease-in-out 5s`);
-    }
-}
-AddAnimation(HeadingAnimation);

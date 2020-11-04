603591b60dfbaf5a828df37ce9d620da0472a6c6
Vikas <vikasmaur10@gmail.com>
2020-11-04 14:15:45 +0530

    Tkinter blog -Button With Hoverable Powers -- Class Added

--- js/app.js
+++ js/app.js
@@ -3,4 +3,7 @@ videos.forEach(video=>{
     let prevTitle = video.getAttribute("title")||"";
     prevTitle+="If the video does not load then please refresh/reload the page";
     video.setAttribute("title",prevTitle);
+    if(!video.getAttribute("preload")){
+        video.setAttribute("preload","none");
+    }
 });

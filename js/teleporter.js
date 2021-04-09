feb9fb1a764b0edd6ff58970144671b10bb6df9c
Vikas <vikasmaur10@gmail.com>
2021-04-09 19:37:06 +0530

    teleporter updated , made the content bit more responsive

---
+++ js/teleporter.js
@@ -0,0 +1,13 @@
+const teleporterButtons = document.querySelectorAll(
+    "#teleporter li button.btn-toggle"
+);
+teleporterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let ariaExpanded = button.getAttribute("aria-expanded");
+        const scrollToId = button.getAttribute("data-targetId");
+        const targetElement = document.getElementById(scrollToId);
+        if (ariaExpanded == "true") {
+            targetElement.scrollIntoView(true);
+        }
+    });
+});

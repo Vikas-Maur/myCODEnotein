4579ea4ad1af011734f0ec31a1b84839de679b55
Vikas <vikasmaur10@gmail.com>
2021-06-03 17:45:38 +0530

    Made myCODEnotein a PWA by adding service worker

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
-SetTeleporterButtons();
-hljs.highlightAll();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("Service worker registered", reg))
+    .catch((err) => console.log("Service worker not registered", err));
+}
+
+let pwaInstallPrompt = null;
+const installPWA = document.getElementById("installPWA");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  pwaInstallPrompt = e;
+});
+
+installPWA.addEventListener("click", (e) => {
+  if (pwaInstallPrompt){
+    pwaInstallPrompt.prompt();
+  }
+});
+
+if (window.matchMedia('(display-mode: standalone)').matches) {  
+  installPWA.remove();
+}  
---
+++ sw.js
@@ -0,0 +1,54 @@
+const staticCacheName = 'site-static-v2';
+const assets = [
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css'
+  ,'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
+  ,'https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap'
+  ,'/logo.png'
+  ,'/logo-big.png'
+  ,'/js/app.js'
+  ,'/js/blog.js'
+  ,'/js/teleporter.js'
+  ,'/js/highlight.pack.js'
+  ,'/index.html'
+  ,'/css/style.css'
+  ,'/css/home.css'
+  ,'/css/highlight/stackoverflow-dark.css'
+  ,'/fallback.html'
+]
+
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request)
+    }).catch(() => {
+      if(evt.request.url.includes('.html')){
+        return caches.match('/fallback.html');
+      } 
+    })
+  );
+});
